e6aa0bb5d4f6636610eab912fa898b058c5eae4c
Steven Haryanto <stevenharyanto@gmail.com>
2012-07-13 12:28:03 +0000

    Import of SHARYANTO/Data-Unixish-1.0.0 from CPAN.
    gitpan-cpan-distribution: Data-Unixish
    gitpan-cpan-version:      1.0.0
    gitpan-cpan-path:         SHARYANTO/Data-Unixish-1.0.0.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Data/Unixish.pm
@@ -0,0 +1,31 @@
+package Data::Unixish;
+
+our $VERSION = '1.0.0'; # VERSION
+
+1;
+# ABSTRACT: Data transformation framework, inspired by Unix toolbox philosophy
+
+__END__
+=pod
+
+=head1 NAME
+
+Data::Unixish - Data transformation framework, inspired by Unix toolbox philosophy
+
+=head1 VERSION
+
+version 1.0.0
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
